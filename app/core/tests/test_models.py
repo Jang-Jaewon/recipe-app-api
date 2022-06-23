674f258d3917275548f9d16a67ba308464d3d987
Jang-Jaewon <jewon119@gmail.com>
2022-06-23 09:09:06 +0900

    Fixed: 오타 수정 및 asserEqual-> asswerTrue로 변경

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,6 @@
 """
 Test for models
 """
-
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -11,12 +10,12 @@ class ModelTests(TestCase):
 
     def teat_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
-        email = "test@email.com"
+        email = "tests@email.com"
         password = "testpassword123"
-        user = get_user_model().objects.crate_user(
+        user = get_user_model().objects.create_user(
             email=email,
             password=password,
         )
 
         self.assertEqual(user.email, email)
-        self.assertEqual(user.check_password(password))
+        self.assertTrue(user.check_password(password))
