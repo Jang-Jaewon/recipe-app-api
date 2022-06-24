fa1314b916b353aa1499fc34610459f1c081c113
Jang-Jaewon <jewon119@gmail.com>
2022-06-24 09:12:15 +0900

    Add: 이메일 주소 정규화 Test 코드 추가

--- app/core/models.py
+++ app/core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
 
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -30,6 +30,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    object = UserManager
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
