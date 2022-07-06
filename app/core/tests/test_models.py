19894bcfc024131590ae11c97881128b405984f3
Jang-Jaewon <jewon119@gmail.com>
2022-07-07 08:52:32 +0900

    Fixed: Recipe 생성 Test 코드 수정 및 Model, Admin 작성

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -63,9 +63,9 @@ class ModelTests(TestCase):
         recipe = models.Recipe.objects.create(
             user=user,
             title="Sample recipe name",
+            time_minutes=5,
             price=Decimal("5.50"),
             description="Sample recipe description."
         )
 
         self.assertEqual(str(recipe), recipe.title)
-        
